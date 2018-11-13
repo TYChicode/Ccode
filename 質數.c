03de8679ed417db700897e76bc2acc85924e751e
TYChicode <44272883+TYChicode@users.noreply.github.com>
2018-11-13 14:02:31 +0800

    Add files via upload

---
+++ 質數.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int n,k,num,j,i,var;
+	scanf ("%d",&n);
+	for (k = 1;k <= n;k++){
+		scanf ("%d",num);
+		if (num == 1){
+			printf ("N");
+		}
+		else {
+			var = 0;
+			for (j = 2;j < num;j++){	
+				if (num % j == 0){
+					var++;
+					break;
+				}
+			}
+			if (var == 0){
+				printf ("Y");
+			}
+		}
+	}
+}
